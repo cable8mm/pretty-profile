70a3d46c16ae1e848825dd0b64f9c63c6dd0dcbd
Samgu Lee <cable8mm@gmail.com>
2026-07-08 21:54:55 +0900

    test: widen test coverage

--- tests/PrettyProfileTest.php
+++ tests/PrettyProfileTest.php
@@ -78,4 +78,74 @@ class PrettyProfileTest extends TestCase
 
         $this->assertEquals('https://cabinet-pets.palgle.com/avatars/cat/1.png', $bg);
     }
+
+    public function test_it_throws_exception_for_invalid_nickname_id(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The value must be over 0, so a value of 0 is not valid.');
+
+        PrettyProfile::getInstance()->nickname(0);
+    }
+
+    public function test_it_throws_exception_for_negative_nickname_id(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The value must be over 0, so a value of -1 is not valid.');
+
+        PrettyProfile::getInstance()->nickname(-1);
+    }
+
+    public function test_it_throws_exception_for_invalid_cat_id(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The value must be over 0, so a value of 0 is not valid.');
+
+        PrettyProfile::getInstance()->cat(0);
+    }
+
+    public function test_it_throws_exception_for_invalid_dog_id(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The value must be over 0, so a value of -5 is not valid.');
+
+        PrettyProfile::getInstance()->dog(-5);
+    }
+
+    public function test_it_throws_exception_for_invalid_cat_size(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The value must be "large" or "medium" or "small", so a value of "invalid" is not valid.');
+
+        PrettyProfile::getInstance()->cat(1, 'invalid');
+    }
+
+    public function test_it_throws_exception_for_invalid_dog_size(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The value must be "large" or "medium" or "small", so a value of "huge" is not valid.');
+
+        PrettyProfile::getInstance()->dog(1, 'huge');
+    }
+
+    public function test_it_throws_exception_for_invalid_animal_in_profile_image(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The value must be dog or cat. rabbit is not valid.');
+
+        PrettyProfile::profileImage(1, animal: 'rabbit');
+    }
+
+    public function test_it_returns_valid_sizes_for_cat(): void
+    {
+        $this->assertStringContainsString('/medium/', PrettyProfile::getInstance()->cat(1, 'medium'));
+        $this->assertStringContainsString('/large/', PrettyProfile::getInstance()->cat(1, 'large'));
+        $this->assertStringContainsString('/small/', PrettyProfile::getInstance()->cat(1, 'small'));
+    }
+
+    public function test_it_returns_valid_sizes_for_dog(): void
+    {
+        $this->assertStringContainsString('/medium/', PrettyProfile::getInstance()->dog(1, 'medium'));
+        $this->assertStringContainsString('/large/', PrettyProfile::getInstance()->dog(1, 'large'));
+        $this->assertStringContainsString('/small/', PrettyProfile::getInstance()->dog(1, 'small'));
+    }
 }
